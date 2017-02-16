7fd2ad3f8438961a121fe4cc821def923ab28471
davijoh73 <davetjohnston@hotmail.com>
2017-02-15 21:49:10 -0600

    Final update to ChangeScene script with variable

--- Assets/ChangeScene.cs
+++ Assets/ChangeScene.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour {
-    public void GoToScene()
+    public void GoToScene(string sceneName)
     {
         Debug.Log("My method was called");
-        SceneManager.LoadScene("00-FallingCoconut");
+        SceneManager.LoadScene(sceneName);
     }
 }
