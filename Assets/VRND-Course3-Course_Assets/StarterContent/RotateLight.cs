936a4e3927cdb70df15ea909de818fadd8b54d92
davijoh73 <davetjohnston@hotmail.com>
2017-02-19 21:40:29 -0600

    Update script with red sunset

---
+++ Assets/VRND-Course3-Course_Assets/StarterContent/RotateLight.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLight : MonoBehaviour {
+
+    public GameObject directionalLight;
+    public Animator sunRotationAnimation;
+    float startTime = 0f;
+    bool isPressed = false;
+
+	// Use this for initialization
+	void Start () {
+        sunRotationAnimation.StartPlayback();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //checking if Triggered is true and assigning it to a variable
+        if (GvrViewer.Instance.Triggered)
+        {
+            isPressed = true;
+        }
+        if (isPressed)
+        {
+            ActivateRotation();
+        }
+        Quaternion startRotation = Quaternion.Euler(50f, 30f, 0f);
+        Quaternion endRotation = startRotation * Quaternion.Euler(0f, 180f, 0f);
+        if (isPressed == true)
+        {
+            sunRotationAnimation.StopPlayback();
+            startTime += Time.deltaTime;
+        }
+	}
+
+    public void ActivateRotation()
+    {
+        isPressed = true;
+        sunRotationAnimation.SetBool("ChangeColor", true);
+    }
+}
